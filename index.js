6ed0d28ad990af85d1c55fda88cce280b6fcc963
Cici Marisa Hardianti <30491674+cicimarisa@users.noreply.github.com>
2022-08-02 15:17:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("submit-btn").addEventListener("click", function(event){
+document.getElementById("submit-btn").addEventListener("submit", function(event){
     event.preventDefault();
     alert("Thanks for visiting. This is a prototype page. It wont submit the data.")
     document.getElementById("form").reset()
